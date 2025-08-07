59d3d703834120498ae6ee358f329a23e5d7364d
Serpico <gregmartintd@gmail.com>
2025-08-07 11:01:22 +0800

    uploads from old pc

--- C++ latte/O^2P/gameCard/main.cpp
+++ C++ latte/O^2P/gameCard/main.cpp
@@ -5,44 +5,78 @@ using namespace std;
 
     // class
 
-    class theGame {
+    class gameCard {
 
         public:
             string title;
             string developer;
             int yearPublished; 
-
+            
             // methods
-            void displayGameCard(string getTitle, string getDeveloper, int getYear) {
-                cout << "game title: " << getTitle << endl;
-                cout << "game developer: " << getDeveloper << endl;
-                cout << "game published: " << getYear << endl;
+            void displayGameCard(string title, string developer, int year) {
+                cout << "game title: " << title << endl;
+                cout << "game developer: " << developer << endl;
+                cout << "game published: " << year << endl;
             }
+
+        };
+
+    class achievement {
+
+        public: 
+
+            string name;
+            string description;
+            int points;
+
+            // constructor
+            achievement (string n, string d, int p) {
+                
+                name = n;
+                description = d;
+                points = p;
+            }
+
+            void displayAchievement () {
+
+                cout << "Achievement: " << name << endl;
+                cout << "Description: " << description << endl;
+                cout << "Points: " << points << endl;
+
+
+            }
+
     };
 
 int main () {
 
     // creating instance of theGame class
 
-    theGame game;
+        gameCard game;
 
-    // creating object from theGame classs
+    // sample IO logic
 
-    cout << "enter the game title: ";
-    
-    getline (cin, game.title);
+        // cout << "enter the game title: ";
+        
+        // getline (cin, game.title);
 
-    cout << "enter the developer of the game: ";
-    
-    getline (cin, game.developer);
-    
-    cout << "enter the year the game was published: ";
+        // cout << "enter the developer of the game: ";
+        
+        // getline (cin, game.developer);
+        
+        // cout << "enter the year the game was published: ";
 
-    cin >> game.yearPublished;
-    cin.ignore();
+        // cin >> game.yearPublished;
+        // cin.ignore();
 
     // display the stuff
-    game.displayGameCard(game.title, game.developer, game.yearPublished);
 
+        // game.displayGameCard(game.title, game.developer, game.yearPublished);
+
+    // trying out contructor
+
+        achievement firstAch ("You only live once!", "Play the game and die once.", 1000);
+
+        firstAch.displayAchievement();
     return 0;
 }
