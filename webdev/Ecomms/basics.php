24adaf5af696fd7bac72572589ddfaf49422c33a
Serpico <gregmartintd@gmail.com>
2025-08-08 11:48:04 +0800

    from old laptop

---
+++ webdev/Ecomms/basics.php
@@ -0,0 +1,137 @@
+<!-- this is me reviewing some html stuff again -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>basics</title>
+    <!-- favicon -->
+    <link rel="icon" href="../Ecomms/assets/img/trollge.png" type="image/x-icons">
+</head>
+<body title="ive changed the color of this thing." style="background-color: khaki">
+    
+    <h1>Welcome to this heading</h1>
+    <h3>Welcome to this heading</h3>
+    <h6>welcome to this heading</h6>
+    <p>this project will be based on me relearning stuff okay?</p>
+    <p>I dont know what I am doing with whats left of my free time.</p>
+    <!-- this is an anchor tag -->  
+    <a href="https://osu.ppy.sh/">click here to go to <b>osu's website!</b></a>
+    <!-- trying out images -->
+    <img src="../Ecomms/assets/img/fire.jpg" alt="fine shyt" width="400" height="300" title="this an image of hanayagi">
+    <h1 style="color: blue">A styled heading with color.</h1>
+    <h2 title="it says it defines an element with the title attribute idk...">Look at this! Hover over this text!</h2>
+    <!-- styled heading modded font size -->
+    <h1 title="tweaked with font size" style="font-size: 60px">This is a modded heading!</h1>
+    <!-- horizontal rule -->
+    <hr title="this is a horizontal rule">
+    <!-- using line breaks -->
+        <p>My name is Khemut <br>
+                and I am going to <br>
+                    beat the fuck out of you!
+        </p>
+
+    <!-- trying out this pre shit -->
+     <pre>
+        It says on the guide
+        
+        that this will display in a fixed format?
+
+        I dont know if that will work.
+
+        That's why im doing this shit.
+     </pre>
+
+     <pre style="color:red;">
+        This should be red.
+
+        If it's not something is 
+
+        Wrong with you!
+     </pre>
+
+     <!-- background colors -->
+     <h2 style="background-color: red;">This should have a red background</h2>
+     <p title = "the font is in verdana." style="background-color:blue; font-family:verdana; font-size: 300%">
+        this paragraph should be blue.
+     </p>
+
+     <!-- text colors -->
+
+     <p style="color: violet; font-family: verdana; font-size: 500%; background-color: gray;">"The text here should be colored, the font color, and size tweaked.</p>
+
+     <!-- text alignment -->
+
+    <p style="text-align: center; color: purple;">this should be centered</p>
+
+    <!-- text formatting -->
+    <hr title="hr cuz below is formatting.">
+
+    <b>this text should be in bold</b> <br>
+
+    <strong>same as bold but signifies strong importance they say</strong> <br>
+
+    <i>this is an italized text</i> <br>
+
+    <em>same with italics but for emphasization</em> <br>
+
+    <small>this text should be small</small> <br>
+
+    <p>this word should be marked: <mark>Khansaa</mark></p>
+
+    <p>this word should lined or deleted idk: <del>Assaaaa</del></p>
+
+    <p><ins>this should be inserted or underlined they say...</ins></p>
+
+    <p>normal text <sub>this is subtexted</sub> just like that</p>
+
+    <p>normal text <sup>this is supertexted</sup> just like that</p>
+
+    <!-- quotation -->
+
+    <p>this is a blockquoted text used with citations</p>
+    <blockquote style="background-color: gray; text-align: center" cite="https://www.facebook.com/martin.ion.58/">Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Ullam cum delectus, vitae officia iste debitis 
+        molestias suscipit quaerat beatae. Totam voluptas dolorem 
+        perferendis placeat nisi numquam, sapiente velit officiis
+        expedita!</blockquote>
+
+    <p>thi is a short quoted text</p>
+    <q>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi blanditiis 
+        aspernatur sunt enim rem 
+        illum molestias repudiandae, totam optio cupiditate magnam exercitationem. 
+        Consectetur laborum amet laboriosam ad! Qui, consectetur totam.</q>
+
+    <p>you should <abbr title="Shut Your Bitch Ass Up">SYBAU</abbr> yeah that text is abbreviated.</p>
+
+    <address>
+
+    308 Negra Arroyo Lane <br>
+    3228 Piermont Dr, Albuquerque, New Mexico
+
+    </address>
+
+    <p>this lyrics <cite><q>little mona lisa lied</q></cite> is from a ptv song.</p>
+
+    <bdo dir="rtl">this should be written from right to left.</bdo> <br>
+
+    <!-- mailing & links-->
+
+
+    <a href="mailto:gregmartintd@gmail.com" target="_blank" style="color: red">Send me something.</a>
+
+    <br>
+
+    <a href="https://github.com/ruoxinshi" target="_blank">
+        <img src="../Ecomms/assets/img/fuyu.jpg" alt="fuyu" width="300px" height="300px">
+    </a>
+
+    <br>
+
+    <img src="../Ecomms/assets/img/monke.gif" alt="neuron activation" style="width: 300px; height: 300px; border: 3px solid black; float: right;">
+    
+
+
+</body>
+</html>
---
+++ webdev/Ecomms/styling.php
@@ -0,0 +1,57 @@
+<!-- this is me following some fundamental css stuff -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>styling</title>
+
+    <!-- external css link -->
+
+    <link rel="stylesheet" href="../Ecomms/css/styling.css">
+
+    <!-- internal css -->
+     <style>
+        
+        body {background-color: gainsboro;}
+        h1 {background-color: purple}
+
+     </style>
+
+</head>
+<body>
+
+    <!-- inline css -->
+
+    <p title="has inline css properties" style="color: khaki; background-color: gray; font-family: verdana; font-size: 300%">this should have the inline css properties I applied</p>
+
+    <hr>
+
+    <!-- internal css -->
+
+    <h1 title="has internal css properties">This heading should have an internal css properties</h1>
+
+    <hr>
+
+    <!-- enternal css -->
+
+    <h2 title="has external css properties">this should have external css properties.</h2>
+
+    <h3>this should have a border.</h3>
+
+    <!-- links with css -->
+
+    <br>
+
+    <a href="https://github.com/ruoxinshi" target="_blank" title="you can click on this">
+        <img src="../Ecomms/assets/img/fuyu.jpg" alt="fuyu" width="300px" height="300px">
+    </a>
+
+    <h3>You can click on this: <a href="https://github.com/ruoxinshi">click me!</a></h3>
+
+    
+
+    
+</body>
+</html>
